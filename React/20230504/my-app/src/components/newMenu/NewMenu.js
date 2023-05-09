a184689a97555b71ce5faf46de701a67f99d85bd
Yun-Seok Lee <ystone0213@naver.com>
2023-05-09 21:42:56 +0900

    230509 COMMIT

---
+++ React/20230504/my-app/src/components/newMenu/NewMenu.js
@@ -0,0 +1,12 @@
+import './NewMenu.css'
+
+function NewMenu() {
+    return (
+        <ul className="newClass">
+            <li>삼선짜장면</li>
+            <li>차돌짬뽕</li>
+        </ul>
+    )
+}
+
+export default NewMenu
