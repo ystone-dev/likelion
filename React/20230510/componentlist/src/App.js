e3a55d666b0ad3dc052f3726518e53a7e5f4c786
Yun-Seok Lee <ystone0213@naver.com>
2023-05-18 23:26:55 +0900

    230518

--- React/20230510/componentlist/src/App.js
+++ React/20230510/componentlist/src/App.js
@@ -1,71 +1,96 @@
-import React from "react";
-import MyList from "./components/MyList";
+import { useState } from "react";
 import Counter from "./components/Counter";
-
-// function Blog(props) {
-//   const sidebar = (
-//     <ul>
-//       {props.posts.map((post) =>
-//         <li key={post.id}>
-//           {post.title}
-//         </li>
-//       )}
-//     </ul>
-//   );
-//   const content = props.posts.map((post) =>
-//     <div key={post.id}>
-//       <h3>{post.title}</h3>
-//       <p>{post.content}</p>
-//     </div>
-//   );
-//   return (
-//     <div>
-//       {sidebar}
-//       <hr />
-//       {content}
-//     </div>
-//   );
-// }
-
-// const posts = [
-//   { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
-//   { id: 2, title: 'Installation', content: 'You can install React from npm.' }
-// ];
 
 function Hello(props) {
   const name = props.name;
-  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //1~10호를 만들 것입니다.
-
-  // list map을 이용하여 h1 태그들의 리스트를 만듭니다.
-  const numComponentsArray = num.map((i) => (
-    <h1 key={i}>
-      안녕, {name} {i}호
-    </h1>
-  ));
-
-  return <div>{numComponentsArray}</div>;
+  if (name) {
+    return <One name={name} />
+  } else {
+    return <Two />
+  }
 }
 
-function One() {
-  return <p>one</p>;
-}
-
-function Two() {
-  return <p>two</p>;
-}
-
-function Three() {
-  return <p>three</p>;
-}
-
-function App() {
+function One(props) {
   return (
     <div>
-      hello world
-      {/* <Blog posts={posts} /> */}
-      <Hello name="재현" />
-      <MyList />
-      <Counter />
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+function Two(props) {
+  return (
+    <div>
+      <h1>name이 입력된 것이 없습니다.</h1>
+    </div>
+  )
+}
+
+// function NoName() {
+//   return (
+//     <div>
+//       <h1>이름을 입력하지 않았습니다.</h1>
+//     </div>
+//   )
+// }
+
+
+
+function App() {
+  const [datas, setDatas] = useState([
+    {
+      title: "개발자 무릎 담요",
+      price: 17500,
+      id: 101,
+    },
+    {
+      title: "Hack Your Life 개발자 노트북 파우치",
+      price: 29000,
+      id: 102,
+    },
+    {
+      title: "우당탕탕 라이켓의 실험실 스티커북",
+      price: 29000,
+      id: 103,
+    },
+    {
+      title: "버그를 Java라 버그잡는 개리씨 키링",
+      price: 29000,
+      id: 104,
+    },
+  ]);
+
+
+  // React는 state 함수의 호출의 일관성을 유지하기 위해서 내부적으로 동일한 내용의 state함수 호출이 여러번 반복되면 하나의 업데이트로 취급하여 처리합니다.
+  function handleClick(id) {
+    // setDatas(datas.filter((item) => {
+    //     return id !== item.id
+    //   }));
+
+    // 함수형 업데이트라고 표현합니다. 함수형 업데이트를 사용하면 함수의 인자로 전달되는 state 값을 React가 이전 state의 값으로 보장합니다. 때문에 상태 업데이트가 비동기적으로 처리된다고 해도 안정적으로 이전 값과 이후 값(버츄얼 돔)을 비겨하여 처리할 수 있습니다.
+    setDatas((prevDatas) => {
+      return prevDatas.filter((item) => {
+        return id !== item.id
+      })
+    });
+  }
+
+  return (
+    <div>
+      <Hello name="gary" />
+
+
+      {/* <Counter />
+      {datas.map((item, index) => {
+        return (
+          <li key={item.id}>
+            <h2>{index + 1}{item.title}</h2>
+            <span>{item.price} 원</span>
+
+            <button onClick={() => { handleClick(item.id) }}>삭제</button>
+          </li>
+        )
+      })} */}
     </div>
   );
 }
